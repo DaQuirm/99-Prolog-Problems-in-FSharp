085593d8e68f36bbe0000c5e64f5949a29056908
DaQuirm <leo.daquirm@gmail.com>
2012-01-22 20:46:31 +0300

    Lists section finished.

--- List.Spec/ListSpec.fs
+++ List.Spec/ListSpec.fs
@@ -89,4 +89,187 @@ let ``When calculating isListPalindrome of [1;4;2;3;2;2;1] it should equal false
   Given [1;4;2;3;2;2;1]
     |> When calculating isListPalindrome
     |> It should equal false
-    |> Verify   
+    |> Verify
+
+(** Problem 1.07 **)
+[<Scenario>]
+let ``When flattening list [a, [b, [c, d], e]] with listFlatten it should equal [a, b, c, d, e]``() =
+  Given [Element 'a'; List[Element 'b'; List[Element 'c'; Element 'd']; Element 'e']]
+    |> When calculating listFlatten
+    |> It should equal ['a'; 'b'; 'c'; 'd'; 'e']
+    |> Verify
+
+(** Problem 1.08 **)
+[<Scenario>]
+let ``When removing consecutive duplicates of the list [a,a,a,a,b,c,c,a,a,d,e,e,e,e] with compress it should equal [a,b,c,a,d,e]``() =
+  Given ['a';'a';'a';'a';'b';'c';'c';'a';'a';'d';'e';'e';'e';'e']
+    |> When calculating compress
+    |> It should equal ['a';'b';'c';'a';'d';'e']
+    |> Verify
+
+(** Problem 1.09 **)
+[<Scenario>]
+let ``When packing list [a,a,a,a,b,c,c,a,a,d,e,e,e,e] with pack it should equal [[a,a,a,a],[b],[c,c],[a,a],[d],[e,e,e,e]]``() =
+  Given ['a';'a';'a';'a';'b';'c';'c';'a';'a';'d';'e';'e';'e';'e']
+    |> When calculating pack
+    |> It should equal [['a';'a';'a';'a'];['b'];['c';'c'];['a';'a'];['d'];['e';'e';'e';'e']]
+    |> Verify
+
+(* Problem 1.10 *)
+[<Scenario>]
+let ``When RLE-encoding list [a,a,a,a,b,c,c,a,a,d,e,e,e,e] with encode it should equal [[4,a],[1,b],[2,c],[2,a],[1,d][4,e]]``() =
+  Given ['a';'a';'a';'a';'b';'c';'c';'a';'a';'d';'e';'e';'e';'e']
+    |> When calculating encode
+    |> It should equal [(4,'a');(1,'b');(2,'c');(2,'a');(1,'d');(4,'e')]
+    |> Verify
+
+(* Problem 1.11 *)
+[<Scenario>]
+let ``When RLE-encoding list [a,a,a,a,b,c,c,a,a,d,e,e,e,e] with encode_modified it should equal [[4,a],b,[2,c],[2,a],d,[4,e]]``() =
+  Given ['a';'a';'a';'a';'b';'c';'c';'a';'a';'d';'e';'e';'e';'e']
+    |> When calculating encode_modified
+    |> It should equal [Tuple (4,'a'); Single 'b'; Tuple (2,'c'); Tuple (2,'a'); Single 'd'; Tuple (4,'e')]
+    |> Verify
+
+(** Problem 1.12 **)
+[<Scenario>]
+let ``When RLE-decoding list [[4,a],b,[2,c],[2,a],d,[4,e]] with decode it should equal [a,a,a,a,b,c,c,a,a,d,e,e,e,e]``() =
+  Given [Tuple (4,'a'); Single 'b'; Tuple (2,'c'); Tuple (2,'a'); Single 'd'; Tuple (4,'e')]
+    |> When calculating decode
+    |> It should equal ['a';'a';'a';'a';'b';'c';'c';'a';'a';'d';'e';'e';'e';'e']
+    |> Verify
+
+(** Problem 1.13 **)
+[<Scenario>]
+let ``When RLE-encoding list [a,a,a,a,b,c,c,a,a,d,e,e,e,e] with encode_direct it should equal [[4,a],b,[2,c],[2,a],d,[4,e]]``() =
+  Given ['a';'a';'a';'a';'b';'c';'c';'a';'a';'d';'e';'e';'e';'e']
+    |> When calculating encode_direct
+    |> It should equal [Tuple (4,'a'); Single 'b'; Tuple (2,'c'); Tuple (2,'a'); Single 'd'; Tuple (4,'e')]
+    |> Verify
+
+(* Problem 1.14 *)
+[<Scenario>]
+let ``When duplicating list [a,b,c,c,d] with dupli it should equal [a,a,b,b,c,c,c,c,d,d]``() =
+  Given ['a';'b';'c';'c';'d']
+    |> When calculating dupli
+    |> It should equal ['a';'a';'b';'b';'c';'c';'c';'c';'d';'d']
+    |> Verify
+
+(** Problem 1.15 **)
+[<Scenario>]
+let ``When calcualting ndupli of list [a,b,c] and 3 it should equal [a,a,a,b,b,b,c,c,c]``() =
+  Given ['a';'b';'c']
+    |> When calculating ndupli 3
+    |> It should equal ['a';'a';'a';'b';'b';'b';'c';'c';'c']
+    |> Verify
+
+(** Problem 1.16 **)
+[<Scenario>]
+let ``When dropping every third element from [a,b,c,d,e,f,g,h,i,k] using drop it should equal [a,b,d,e,g,h,k]``() =
+  Given ['a';'b';'c';'d';'e';'f';'g';'h';'i';'k']
+    |> When calculating drop 3
+    |> It should equal ['a';'b';'d';'e';'g';'h';'k']
+    |> Verify
+
+(** Problem 1.17 **)
+[<Scenario>]
+let ``When splitting list [a,b,c,d,e,f,g,h,i,k] using split at position 3 it should equal [a,b,c],[d,e,f,g,h,i,k]``() =
+  Given ['a';'b';'c';'d';'e';'f';'g';'h';'i';'k']
+    |> When calculating split 3
+    |> It should equal (['a';'b';'c'], ['d';'e';'f';'g';'h';'i';'k'])
+    |> Verify
+
+(** Problem 1.18 **)
+[<Scenario>]
+let ``When slicing list [a,b,c,d,e,f,g,h,i,k] using slice from 3 to 7 it should equal [c,d,e,f,g]``() =
+  Given ['a';'b';'c';'d';'e';'f';'g';'h';'i';'k']
+    |> When calculating slice 3 7
+    |> It should equal ['c';'d';'e';'f';'g']
+    |> Verify
+
+(** Problem 1.19 **)
+[<Scenario>]
+let ``When rotating list [a,b,c,d,e,f,g,h] using rotate by 3 it should equal [d,e,f,g,h,a,b,c]``() =
+  Given ['a';'b';'c';'d';'e';'f';'g';'h']
+    |> When calculating rotate 3
+    |> It should equal ['d';'e';'f';'g';'h';'a';'b';'c']
+    |> Verify
+
+[<Scenario>]
+let ``When rotating list [a,b,c,d,e,f,g,h] using rotate by -2 it should equal [g,h,a,b,c,d,e,f]``() =
+  Given ['a';'b';'c';'d';'e';'f';'g';'h']
+    |> When calculating rotate -2
+    |> It should equal ['g';'h';'a';'b';'c';'d';'e';'f']
+    |> Verify
+
+(* Problem 1.20 *)
+[<Scenario>]
+let ``When removing element at position 2 of list [a,b,c,d] using remove_at it should equal b,[a,c,d]``() =
+  Given ['a';'b';'c';'d']
+    |> When calculating remove_at 2
+    |> It should equal ('b', ['a';'c';'d'])
+    |> Verify
+
+(* Problem 1.21 *)
+[<Scenario>]
+let ``When inserting element α at position 2 of list [a,b,c,d] using insert_at it should equal [a,α,b,c,d]``() =
+  Given ['a';'b';'c';'d']
+    |> When calculating insert_at 'α' 2
+    |> It should equal ['a';'α';'b';'c';'d']
+    |> Verify
+
+(* Problem 1.22 *)
+[<Scenario>]
+let ``When creating a range from 4 to 9 using range it should equal [4,5,6,7,8,9]``() =
+    When calculating range 4 9
+    |> It should equal [4;5;6;7;8;9]
+    |> Verify
+
+(* Problem 1.23 *)
+
+
+(* Problem 1.24 *)
+
+
+(* Problem 1.25 *)
+
+
+(** Problem 1.26 **)
+[<Scenario>]
+let ``When generating combinations of list [a,b,c,d] elements using combination of 2 it should equal [[a,b],[a,c],[a,d],[b,c],[b,d],[c,d]]``() =
+  Given ['a';'b';'c';'d']
+    |> When calculating combination 2
+    |> It should equal [['a';'b'];['a';'c'];['a';'d'];['b';'c'];['b';'d'];['c';'d']]
+    |> Verify
+
+(** Problem 1.27a **)
+[<Scenario>]
+let ``When generating combinations of disjoint subsets of list [a,b,c,d,e,f,g,h,i] elements using group234 the combination count should equal 1260``() =
+  Given ['a';'b';'c';'d';'e';'f';'g';'h';'i']
+    |> When calculating group234 |> List.length
+    |> It should equal 1260
+    |> Verify
+
+(** Problem 1.27b **)
+[<Scenario>]
+let ``When generating combinations of disjoint subsets of list [a,b,c,d,e] elements using combination of [1;2;1] the combination count should equal 60``() =
+  Given ['a';'b';'c';'d';'e']
+    |> When calculating group_comb [1;2;1] |> List.length
+    |> It should equal 60
+    |> Verify
+
+(** Problem 1.28a **)
+[<Scenario>]
+let ``When sorting a list of lists [[a,b,c],[d,e],[f,g,h],[d,e],[i,j,k,l],[m,n],[o]] according to length of sublists with lsort it should equal [[o],[d,e],[d,e],[m,n],[a,b,c],[f,g,h],[i,j,k,l]]``() =
+  Given [['a';'b';'c'];['d';'e'];['f';'g';'h'];['d';'e'];['i';'j';'k';'l'];['m';'n'];['o']]
+    |> When calculating lsort
+    |> It should equal [['o'];['d';'e'];['d';'e'];['m';'n'];['a';'b';'c'];['f';'g';'h'];['i';'j';'k';'l']]
+    |> Verify
+
+(** Problem 1.28b **)
+[<Scenario>]
+let ``When sorting a list of lists [[a,b,c],[d,e],[f,g,h],[d,e],[i,j,k,l],[m,n],[o]] according to their length frequency with lfsort it should equal [[i,j,k,l],[o],[a,b,c],[f,g,h],[d,e],[d,e],[m,n]]``() =
+  Given [['a';'b';'c'];['d';'e'];['f';'g';'h'];['d';'e'];['i';'j';'k';'l'];['m';'n'];['o']]
+    |> When calculating lfsort
+    |> It should equal [['i';'j';'k';'l'];['o'];['a';'b';'c'];['f';'g';'h'];['d';'e'];['d';'e'];['m';'n']]
+    |> Verify
--- Lists/ListProblems.fs
+++ Lists/ListProblems.fs
@@ -7,6 +7,8 @@ let lastElement list =
 
 (* Problem 1.02 *)
 // Find the last but one element of a list.
+// (de: zweitletztes Element, fr: avant-dernier élément)
+// DaQuirm's comment: (en: penultimate element)
 let lastButOneElement list = 
     List.rev list |> List.tail |> List.head
 
@@ -36,3 +38,218 @@ let isListPalindrome list =
 // Flatten a nested list structure.
 // Transform a list, possibly holding lists as elements into a 'flat' list 
 // by replacing each list with its elements (recursively).
+// Hint: Use the predefined predicates is_list/1 and append/3
+type NestedList<'T> = 
+    | Element of 'T
+    | List of NestedList<'T> list
+
+let rec listFlatten list = 
+    [for item in list do 
+        match item with 
+        | List l -> yield! listFlatten l
+        | Element e -> yield e
+    ]
+
+(** Problem 1.08 **) 
+// Eliminate consecutive duplicates of list elements. 
+// If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
+let compress list =
+    List.foldBack (fun item acc -> 
+        match acc with
+        | [] -> [item]
+        | head :: _ when head <> item -> item :: acc
+        | _ -> acc
+    ) list []
+
+(** Problem 1.09 **)
+// Pack consecutive duplicates of list elements into sublists. 
+// If a list contains repeated elements they should be placed in separate sublists.
+let pack list =
+    List.foldBack (fun item acc -> 
+        match acc with
+        | [] -> [[item]]
+        | headList :: _ when headList.Head <> item -> [item] :: acc
+        | headList :: tail when headList.Head = item -> (item :: headList) :: tail
+        | _ -> acc
+    ) list []
+
+(* Problem 1.10 *)
+// Run-length encoding of a list. 
+// Use the result of problem 1.09 to implement the so-called run-length encoding data compression method. 
+// Consecutive duplicates of elements are encoded as terms [N,E] where N is the number of duplicates of the element E.
+let encode list =
+    list |> (pack >> List.map (fun list -> list.Length, list.Head))
+
+(* Problem 1.11 *)
+// Modified run-length encoding.
+// Modify the result of problem 1.10 in such a way that if an element has no duplicates it is simply copied into the result list.
+// Only elements with duplicates are transferred as [N,E] terms.
+type RleList<'T> =     
+    | Single of 'T
+    | Tuple of int * 'T 
+
+let encode_modified list =
+    list |> pack |> List.map (fun list -> 
+        match list.Length with
+        | 1 -> Single list.Head
+        | _ -> Tuple (list.Length, list.Head)
+    )
+
+(** Problem 1.12 **)
+// Decode a run-length encoded list. 
+// Given a run-length code list generated as specified in problem 1.11. Construct its uncompressed version.
+let decode list =
+    List.collect (fun item ->
+        match item with
+        | Single value -> [value]
+        | Tuple (length, value) -> List.replicate length value
+    ) list
+
+(** Problem 1.13 **) 
+// Run-length encoding of a list (direct solution).
+// Implement the so-called run-length encoding data compression method directly.
+// I.e. don't explicitly create the sublists containing the duplicates, as in problem 1.09, but only count them.
+// As in problem 1.11, simplify the result list by replacing the singleton terms [1,X] by X.
+let encode_direct list =
+    List.foldBack (fun item acc -> 
+        match acc with
+        | [] -> [Single item]
+        | head :: tail ->
+            match head with
+            | Single value when value = item -> Tuple (2, value) :: tail
+            | Tuple (length, value) when value = item -> Tuple (length + 1, value) :: tail
+            | _ -> Single item :: acc
+    ) list [] 
+
+(* Problem 1.14 *)
+// Duplicate the elements of a list.
+let dupli list =
+    List.collect (fun item -> [item; item]) list
+
+(** Problem 1.15 **)
+// Duplicate the elements of a list a given number of times.
+let ndupli n list = 
+    List.collect (fun item -> List.replicate n item) list
+
+(** Problem 1.16 **) 
+// Drop every N'th element from a list.
+let drop n list =
+    list |> List.mapi (fun index item -> item, index)
+         |> List.filter (fun (item, index) -> (index + 1) % n <> 0)
+         |> List.map fst
+
+(* Problem 1.17 *)
+// Split a list into two parts; the length of the first part is given. 
+// Do not use any predefined predicates.
+let split length list =
+    list |> List.mapi (fun index item -> item, index)
+         |> List.partition (fun (item, index) -> index < length)
+         |> (fun (left, right) -> ((left |> List.map fst), (right |> List.map fst)))
+
+(** Problem 1.18 **)
+// Extract a slice from a list.
+// Given two indices, I and K, the slice is the list containing the elements between the I'th and K'th element of the original list
+// (both limits included). Start counting the elements with 1.
+let slice i k list =
+    (Seq.skip (i - 1) >> Seq.take (k - i + 1) >> Seq.toList) list
+
+(** Problem 1.19 **)
+// Rotate a list N places to the left.
+// Hint: Use the predefined predicates length/2 and append/3, as well as the result of problem 1.17.
+let rotate n list =
+    let length = List.length list
+    split ((n + length) % length) list |> (fun (left, right) -> right @ left)
+
+(* Problem 1.20 *)
+// Remove the K'th element from a list.
+let remove_at position list =
+    (split (position - 1) list) |> (fun (left, right) -> right.Head, left @ right.Tail)
+
+(* Problem 1.21 *)
+// Insert an element at a given position into a list.
+let insert_at item position list =
+    (split (position - 1) list) |> (fun (left, right) -> left @ item :: right)
+
+(* Problem 1.22 *)
+// Create a list containing all integers within a given range.
+let range first last =
+    [first..last]
+
+(** Problem 1.23 **)
+// Extract a given number of randomly selected elements from a list. 
+// The selected items shall be put into a result list.
+// Hint: Use the built-in random number generator random/2 and the result of problem 1.20.
+let rnd = new System.Random();
+
+let rnd_select n list =
+    List.sortWith (fun item1 item2 -> rnd.Next(0, 2) * 2 - 1) list
+    |> Seq.take n |> Seq.toList
+
+(* Problem 1.24 *)
+// Lotto: Draw N different random numbers from the set 1..M.
+// Hint: Combine the solutions of problems 1.22 and 1.23.
+let lotto_select n first last =
+    rnd_select n [first..last]
+
+(* Problem 1.25 *)
+// Generate a random permutation of the elements of a list.
+// Hint: Use the solution of problem 1.23.
+let rnd_permu list =
+    List.sortWith (fun item1 item2 -> rnd.Next(0, 2) * 2 - 1) list
+
+(** Problem 1.26 **)
+// Generate the combinations of K distinct objects chosen from the N elements of a list
+// In how many ways can a committee of 3 be chosen from a group of 12 people?
+// We all know that there are C(12,3) = 220 possibilities (C(N,K) denotes the well-known binomial coefficients).
+// For pure mathematicians, this result may be great. But we want to really generate all the possibilities (via backtracking).
+//TODO: improve by adding a more general match pattern
+let rec combination m list =
+    match (m, list) with
+    | (0,_) -> [[]]
+    | (_,[]) -> []
+    | (n, head :: tail) -> List.map (fun item -> head :: item) (combination (n-1) tail) @ combination n tail
+
+(** Problem 1.27 **)
+// Group the elements of a set into disjoint subsets. 
+// a) In how many ways can a group of 9 people work in 3 disjoint subgroups of 2, 3 and 4 persons? 
+// Write a predicate that generates all the possibilities via backtracking.
+// b) Generalize the above predicate in a way that we can specify a list of group sizes and the predicate will return a list of groups.
+// Note that we do not want permutations of the group members; 
+// i.e. [[aldo,beat],...] is the same solution as [[beat,aldo],...]. 
+// However, we make a difference between [[aldo,beat],[carla,david],...] and [[carla,david],[aldo,beat],...].
+let rec sieve_combination m alist plist =
+    match (m, alist) with
+    | (0,_) -> [[], plist @ alist]
+    | (_,[]) -> []
+    | (n, head::tail) -> List.map (fun (comb, unused) -> (head :: comb, unused)) 
+                                  (sieve_combination (n-1) tail plist) @ sieve_combination n tail (head :: plist)
+
+let rec group_comb groups list =
+    match groups with
+    | [] -> [[]]
+    | head :: tail -> 
+        (sieve_combination head list []) 
+        |> List.collect (
+            fun (comb, unused) -> 
+                List.map (fun solution -> comb :: solution) (group_comb tail unused)
+        )
+
+let group234 list = group_comb [2;3;4] list
+
+(** Problem 1.28a **)
+// Sorting a list of lists according to length of sublists
+// a) We suppose that a list (InList) contains elements that are lists themselves.
+// The objective is to sort the elements of InList according to their length. 
+// E.g. short lists first, longer lists later, or vice versa.
+let lsort list =
+    List.sortBy (fun item -> List.length item) list
+
+(** Problem 1.28b **)
+// b) Again, we suppose that a list (InList) contains elements that are lists themselves.
+// But this time the objective is to sort the elements of InList according to their length frequency;
+// i.e. in the default, where sorting is done ascendingly, lists with rare lengths are placed first, others with a more frequent length come later.
+let lfsort list =
+    Seq.groupBy List.length list 
+    |> Seq.map (snd >> List.ofSeq) 
+    |> Seq.sortBy Seq.length 
+    |> List.concat;;
